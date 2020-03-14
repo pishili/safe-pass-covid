561b499a16cbb0b2d0b555c3cab0c6fcf890ce36
pishili <pishili@users.noreply.github.com>
2020-03-14 01:47:27 -0700

    seeds members database

---
+++ api/database/seeds/02_seeds.sql
@@ -0,0 +1,10 @@
+\c project;
+
+  INSERT INTO members (
+    name, email, password) 
+    VALUES (
+    'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+    INSERT INTO members (
+    name, email, password) 
+    VALUES (
+    'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
