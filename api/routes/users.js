f12e5896c2de5815c699c379fe09444c49ced14b
pishili <pishili@users.noreply.github.com>
2020-03-14 01:29:20 -0700

    Adds users get endpoint

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,9 +2,30 @@ var express = require('express');
 var router = express.Router();
 const { Pool } = require('pg');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const pool = new Pool({
+  user: 'postgres',
+  password: 'example',
+  host: 'localhost',
+  port: 5672,
+  database: 'project'
+})
+
+router.get("/", (req, res, next) => {
+  pool.query(`
+  SELECT name, email
+  FROM users
+  LIMIT 5;
+`)
+    .then(result => {
+      console.log(result.rows.length);
+      res.send(result.rows)
+    })
+    .catch(err => {
+      console.error('query error', err.stack)
+      res.send("ERROR");
+    });
+
 });
+
 
 module.exports = router;
