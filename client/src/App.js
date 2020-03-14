e1975fa9a1afcae0a879b6bd59fd9ac63d87468d
pishili <pishili@users.noreply.github.com>
2020-03-14 01:17:51 -0700

    installed pg client and added pool instance

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = useState({
+    apiResponse: ""
+  })
+
+  const callAPI = () => {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => {
+        console.log(res)
+        this.setState({ apiResponse: res })
+      })
+      .catch(err => err);
+  }
+
+  useEffect(() => {
+    callAPI()
+  }, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to React</h1>
       </header>
     </div>
   );
