f9d3d7a2272f698e6362595cfff0ca4e3af45ff7
pishili <pishili@users.noreply.github.com>
2020-03-15 11:03:20 -0700

    Adds membersRouter to app in api

--- api/app.js
+++ api/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var membersRouter = require('./routes/members');
 var testAPIRouter = require('./routes/testAPI');
 
 var app = express();
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/members', membersRouter);
 app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
