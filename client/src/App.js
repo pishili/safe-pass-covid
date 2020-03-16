af46b201d9e2d99dc1716a9963545c36b5922d79
pishili <pishili@users.noreply.github.com>
2020-03-15 19:04:54 -0700

    Adds container

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,8 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 
 const axios = require('axios');
@@ -110,60 +112,60 @@ function App() {
 
   }, [setViewport.latitude, setViewport.longitude])
 
-
-
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3} alignItems="center" alignContent="center" >
-        <Grid item xs={12}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" className={classes.title}>
-                SafePass
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth={false}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                  SafePass
           </Typography>
-              <Button color="inherit">Login</Button>
-            </Toolbar>
-          </AppBar>
-        </Grid>
-        <Grid item xs={3} />
-        <Grid item xs={6}>
-          <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  {state.columns.map(col => <TableCell>{col}</TableCell>)}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {state.members.map(row => (
+                <Button color="inherit">Login</Button>
+              </Toolbar>
+            </AppBar>
+          </Grid>
+          <Grid item xs={3} />
+          <Grid item xs={6}>
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="simple table">
+                <TableHead>
                   <TableRow>
-                    <TableCell>{row.name}</TableCell>
-                    <TableCell>{row.age}</TableCell>
-                    <TableCell>{row.location}</TableCell>
+                    {state.columns.map(col => <TableCell>{col}</TableCell>)}
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                </TableHead>
+                <TableBody>
+                  {state.members.map(row => (
+                    <TableRow>
+                      <TableCell>{row.name}</TableCell>
+                      <TableCell>{row.age}</TableCell>
+                      <TableCell>{row.location}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
+          <Grid item xs={3} />
+          <Grid item xs={2} />
+          <Grid container item xs={8} justify="center">
+            <ReactMapGL
+              mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+              {...viewport}
+              onViewportChange={setViewport}
+            >
+              {cityMarkers}
+            </ReactMapGL>
+          </Grid>
+          <Grid item xs={2} />
         </Grid>
-        <Grid item xs={3} />
-        <Grid item xs={2} />
-        <Grid container item xs={8} justify="center">
-          <ReactMapGL
-            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-            {...viewport}
-            onViewportChange={setViewport}
-          >
-            {cityMarkers}
-          </ReactMapGL>
-        </Grid>
-        <Grid item xs={2} />
-      </Grid>
-
-    </div>
+      </Container>
+    </React.Fragment>
   );
 }
 
