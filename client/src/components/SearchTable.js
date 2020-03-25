191ebba9cee687971746d99d063c42cebc8935e1
pishili <pishili@users.noreply.github.com>
2020-03-25 00:06:58 -0700

    Adds data, setState and useEffect in SearchTable component

--- client/src/components/SearchTable.js
+++ client/src/components/SearchTable.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { AddBox, ArrowDownward } from "@material-ui/icons";
 import MaterialTable from "material-table";
 
 export default function SearchTable(props) {
+
+  const data = [
+    {
+      "id": 1,
+      "vendor_id": 1,
+      "location": "Downtown",
+      "capacity": "500"
+    },
+    {
+      "id": 2,
+      "vendor_id": 3,
+      "location": "Downtown",
+      "capacity": "100"
+    }
+  ]
+
+  const [state, setState] = useState({
+    tableData: [],
+    isLoading: false
+  })
+
+  useEffect(() => {
+    setState((prev) => ({ ...prev, ...{ isLoading: true} }))
+    setTimeout(() => {
+      setState((prev) => ({ ...prev, ...{ tableData: data, isLoading: false} }))
+    }, 2000);
+
+  }, [])
+
   return (
     <div style={{ maxWidth: "100%" }}>
       <MaterialTable
         columns={[
-          { title: "Adı", field: "name" },
-          { title: "Soyadı", field: "surname" },
-          { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
-          {
-            title: "Doğum Yeri",
-            field: "birthCity",
-            lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
-          }
+          { title: "VendorID", field: "vendor_id" },
+          { title: "Location", field: "location" },
         ]}
-        data={[
-          { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 }
-        ]}
+        data={state.tableData}
+        isLoading={state.isLoading}
         title="Demo Title"
       />
     </div>
