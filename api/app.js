f64854495a47d5ce3c57c4082719d7aff7182bef
pishili <pishili@users.noreply.github.com>
2020-03-25 00:04:17 -0700

    Adds stores route to app API

--- api/app.js
+++ api/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var membersRouter = require('./routes/members');
 var testAPIRouter = require('./routes/testAPI');
+var storesRouter = require('./routes/stores');
 
 var app = express();
 
@@ -25,6 +26,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/members', membersRouter);
 app.use("/testAPI", testAPIRouter);
+app.use("/stores", storesRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
