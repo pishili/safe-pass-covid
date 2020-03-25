d7e3fc9041ee68792f7b48dca14b2284e52c8720
pishili <pishili@users.noreply.github.com>
2020-03-24 23:22:00 -0700

    Adds stores api

---
+++ api/routes/stores.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'example',
+    host: 'localhost',
+    port: 5672,
+    database: 'project'
+})
+
+
+router.get("/", (req, res, next) => {
+  pool.query(`
+  SELECT *
+  FROM stores;
+`)
+    .then(result => {
+      console.log(result.rows.length);
+      res.send(result.rows)
+    })
+    .catch(err => {
+      console.error('query error', err.stack)
+      res.send("ERROR");
+    });
+
+});
+
+module.exports = router;
