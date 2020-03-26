9c6ba7df531a07996888d6ad1daea15588d504d1
pishili <pishili@users.noreply.github.com>
2020-03-25 22:43:58 -0700

    adds info to have two reservations of the same customer

--- api/database/seeds/06_reservations.sql
+++ api/database/seeds/06_reservations.sql
@@ -1,10 +1,14 @@
 \c project;
 
   INSERT INTO reservations (
-    customer_id, store_id, visting_day, visting_hour) 
+    customer_id, store_id, visiting_day, visiting_hour) 
     VALUES (
     2, 1, 90, 8);
     INSERT INTO reservations (
-    customer_id, store_id, visting_day, visting_hour) 
+    customer_id, store_id, visiting_day, visiting_hour) 
     VALUES (
     1, 1, 90, 10);
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    2, 1, 90, 10);
