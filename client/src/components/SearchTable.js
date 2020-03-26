ebc78151fcaf088897d9f5a0dfcff32ac312c2ee
pishili <pishili@users.noreply.github.com>
2020-03-25 19:13:21 -0700

    Adds name to the Demo table

--- client/src/components/SearchTable.js
+++ client/src/components/SearchTable.js
@@ -5,45 +5,23 @@ import MaterialTable from "material-table";
 
 export default function SearchTable(props) {
 
-  // const data = [
-  //   {
-  //     "id": 1,
-  //     "vendor_id": 1,
-  //     "location": "Downtown",
-  //     "capacity": "500"
-  //   },
-  //   {
-  //     "id": 2,
-  //     "vendor_id": 3,
-  //     "location": "Downtown",
-  //     "capacity": "100"
-  //   }
-  // ]
+  const [state, setState] = useState({
+    tableData: [],
+    isLoading: false
+  })
 
   const callAPIStores = () => {
     fetch("/stores")
       .then(res => res.text())
       .then(res => {
         let stores = JSON.parse(res)
-          // .map((i) => {
-          //   return {
-          //     vendor_id: i.vendor_id,
-          //     location: i.location,
-          //     capacity: i.capacity
-          //   }
-          })
-          setState(prev => ({ ...prev, ...{ tableData: stores, isLoading: false } }))
+        setState(prev => ({ ...prev, ...{ tableData: stores, isLoading: false } }))
       })
       .catch(err => err);
   }
 
-  const [state, setState] = useState({
-    tableData: [],
-    isLoading: false
-  })
-
   useEffect(() => {
-    setState((prev) => ({ ...prev, ...{ isLoading: true} }))
+    setState((prev) => ({ ...prev, ...{ isLoading: true } }))
     setTimeout(() => {
       callAPIStores()
     }, 2000);
@@ -55,8 +33,9 @@ export default function SearchTable(props) {
       <MaterialTable
         columns={[
           { title: "VendorID", field: "vendor_id" },
+          { title: "Name", field: "name" },
           { title: "Location", field: "location" },
-          { title: "Capacity", field: "capacity" },
+          { title: "Capacity", field: "capacity" }
         ]}
         data={state.tableData}
         isLoading={state.isLoading}
