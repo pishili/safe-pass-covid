a5fbf55e318b4f6715d9af2a598ef73ecdba1e13
pishili <pishili@users.noreply.github.com>
2020-03-25 18:18:40 -0700

    Fixes callAPIStores function for fetching stores

--- client/src/components/SearchTable.js
+++ client/src/components/SearchTable.js
@@ -5,39 +5,40 @@ import MaterialTable from "material-table";
 
 export default function SearchTable(props) {
 
-  const data = [
-    {
-      "id": 1,
-      "vendor_id": 1,
-      "location": "Downtown",
-      "capacity": "500"
-    },
-    {
-      "id": 2,
-      "vendor_id": 3,
-      "location": "Downtown",
-      "capacity": "100"
-    }
-  ]
+  // const data = [
+  //   {
+  //     "id": 1,
+  //     "vendor_id": 1,
+  //     "location": "Downtown",
+  //     "capacity": "500"
+  //   },
+  //   {
+  //     "id": 2,
+  //     "vendor_id": 3,
+  //     "location": "Downtown",
+  //     "capacity": "100"
+  //   }
+  // ]
 
-  // const callAPIStores = () => {
-  //   fetch("/stores")
-  //     .then(res => res.text())
-  //     .then(res => {
-  //       let stores = JSON.parse(res)
-  //         .map((i) => {
-  //           return {
-  //             vendor_id: i.vendor_id,
-  //             location: i.location
-  //           }
-  //         })
-  //       const columns = ["Vendor_id", "Location"]
-  //       setState(prev => ({ ...prev, ...{ stores, columns } }))
-  //     })
-  //     .catch(err => err);
-  // }
+  const callAPIStores = () => {
+    fetch("/stores")
+      .then(res => res.text())
+      .then(res => {
+        let stores = JSON.parse(res)
+          .map((i) => {
+            return {
+              vendor_id: i.vendor_id,
+              location: i.location,
+              capacity: i.capacity
+            }
+          })
+          setState(prev => ({ ...prev, ...{ tableData: stores, isLoading: false } }))
+      })
+      .catch(err => err);
+  }
 
-  // const data = callAPIStores()
+  // const data = callAPIStores();
+  // console.log(data)
 
   const [state, setState] = useState({
     tableData: [],
@@ -47,7 +48,7 @@ export default function SearchTable(props) {
   useEffect(() => {
     setState((prev) => ({ ...prev, ...{ isLoading: true} }))
     setTimeout(() => {
-      setState((prev) => ({ ...prev, ...{ tableData: data, isLoading: false} }))
+      callAPIStores()
     }, 2000);
 
   }, [])
@@ -58,6 +59,7 @@ export default function SearchTable(props) {
         columns={[
           { title: "VendorID", field: "vendor_id" },
           { title: "Location", field: "location" },
+          { title: "Capacity", field: "capacity" },
         ]}
         data={state.tableData}
         isLoading={state.isLoading}
