7a4767bb7de9bac04516e546d199b50527e6dbe1
pishili <pishili@users.noreply.github.com>
2020-03-27 18:04:52 -0700

    Adds more stores

--- api/database/seeds/05_stores.sql
+++ api/database/seeds/05_stores.sql
@@ -16,3 +16,120 @@
     location, vendor_id, capacity)
     VALUES (
     'NorthVan', 2, 80);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Downtown', 1, 500);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Downtown', 1, 500);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'MetroTown', 1, 50);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'PortCoquitlam', 1, 200);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Surrey', 1, 300);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Richmond', 1, 250);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'NorthVan', 2, 80);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Richmond', 1, 250);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'NorthVan', 2, 80);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Surrey', 2, 100);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Coquitlam', 2, 75);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Burnaby', 2, 90);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Vancouver', 2, 60);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'NorthVan', 3, 80);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'NorthVan', 3, 80);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Vancouver', 3, 90);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Burnaby', 3, 60);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Coquitlam', 3, 50);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Surrey', 3, 75);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'NorthVan', 4, 80);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Vancouver', 4, 90);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Burnaby', 4, 60);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Coquitlam', 4, 50);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Surrey', 4, 85);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'NorthVan', 5, 80);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Vancouver', 5, 100);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Burnaby', 5, 70);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Coquitlam', 5, 60);
+    INSERT INTO stores (
+    location, vendor_id, capacity)
+    VALUES (
+    'Surrey', 5, 75);
+    
