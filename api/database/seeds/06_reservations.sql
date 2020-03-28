d8e03ac0e2c4471edecac2284190c396adc437c5
pishili <pishili@users.noreply.github.com>
2020-03-27 18:05:12 -0700

    Adds more reservations

--- api/database/seeds/06_reservations.sql
+++ api/database/seeds/06_reservations.sql
@@ -3,12 +3,28 @@
   INSERT INTO reservations (
     customer_id, store_id, visiting_day, visiting_hour) 
     VALUES (
-    2, 1, 90, 8);
+    2, 1, 90, '8 AM');
     INSERT INTO reservations (
     customer_id, store_id, visiting_day, visiting_hour) 
     VALUES (
-    1, 1, 90, 10);
+    1, 1, 90, '10 AM');
     INSERT INTO reservations (
     customer_id, store_id, visiting_day, visiting_hour) 
     VALUES (
-    2, 1, 90, 10);
+    2, 2, 90, '2 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    2, 2, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    2, 2, 90, '1 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    2, 3, 90, '10 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    2, 2, 90, '4 PM');
