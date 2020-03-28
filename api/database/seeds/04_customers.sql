c343b450e28785ce8a0d25eb47f75138dec9b2b8
pishili <pishili@users.noreply.github.com>
2020-03-28 10:13:09 -0700

    Adds more customers to database

--- api/database/seeds/04_customers.sql
+++ api/database/seeds/04_customers.sql
@@ -8,3 +8,77 @@
     name, age) 
     VALUES (
     'Iva Harrison', 50);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'Ladan Shabani', 31);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'Hamed Sirvani', 38);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'Lisa Ebar', 54);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'Ali Rostami', 24);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'Nazi Bayat', 20);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'Parasto Jalali', 44);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'PARISA EGHBALIAN', 29);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'REERA ESMAEILION', 10);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'Shakiba Ghahreman', 30);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'Shakiba Ghahreman', 30);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'GHANIMAT AZHDAR', 36);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'NASER POURSHABANOSHIB', 45);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'FIROUZEH MADAN', 44);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'POUNEH GORJI', 26);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'ARASH POURZARABI', 26);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'FIROUZEH MADAN', 44);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'FIROUZEH MADAN', 44);
+    INSERT INTO customers (
+    name, age) 
+    VALUES (
+    'ELNAZ NABIYI', 27);
+    
+    
