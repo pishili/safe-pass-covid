2811a1194bd3276fb7c6d2eb9a9eb71e2249240e
pishili <pishili@users.noreply.github.com>
2020-03-28 13:17:49 -0700

    Adds reservations

--- api/database/seeds/06_reservations.sql
+++ api/database/seeds/06_reservations.sql
@@ -28,3 +28,161 @@
     customer_id, store_id, visiting_day, visiting_hour) 
     VALUES (
     2, 2, 90, '4 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    3, 2, 90, '4 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    4, 2, 90, '4 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    5, 2, 90, '4 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    6, 2, 90, '4 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    7, 2, 90, '4 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    8, 2, 90, '4 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    9, 2, 90, '4 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    2, 2, 90, '1 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    1, 2, 90, '1 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    4, 2, 90, '1 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    5, 2, 90, '1 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    6, 2, 90, '1 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    8, 2, 90, '1 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    1, 4, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    2, 4, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    3, 4, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    4, 4, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    5, 4, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    6, 4, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    7, 4, 90, '5 PM');INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    8, 4, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    9, 4, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    10, 4, 90, '5 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    3, 6, 90, '9 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    1, 6, 90, '9 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    5, 6, 90, '9 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    6, 6, 90, '9 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    3, 6, 90, '9 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    7, 6, 90, '9 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    1, 33, 90, '11 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    2, 33, 90, '11 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    3, 33, 90, '11 AM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    2, 34, 90, '1 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    6, 34, 90, '1 PM');
+    INSERT INTO reservations (
+    customer_id, store_id, visiting_day, visiting_hour) 
+    VALUES (
+    5, 34, 90, '1 PM');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
